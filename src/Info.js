73fe27aae714a2bede436c73e9964ad0ef160eb9
Parvez Ahmed <parvezahmed0786@gmail.com>
2021-09-15 01:37:43 +0530

    Add files via upload

---
+++ src/Info.js
@@ -0,0 +1,42 @@
+const info=[
+
+    {
+        id:1,
+        time: "2020-Present",
+        para: [<p>I am an undergraduate pursuing <span>B.Tech (Mathematics and Computing) at Delhi Technological University. I scored 9.4 SGPA</span> in first semester. I have learned frontend web development. I am pretty good at making landing-pages.</p>],
+        direction: "left",
+        img: "./images/student.png"
+    },
+    {
+        id:2,
+        time:"2018-2020",
+        para:[<p>I completed my senior secondary from Red Roses Public School, Saket. I scored a pretty decent score of <span>95.2%</span> in 10+2 board examinations.</p>],
+        direction:"right",
+        img:"./images/notebook.png"
+    },
+    {
+        id:3,
+        time:"2016-2018",
+        para:[<p>I scored 89.8% in 10th standard board examinations. Actively participated in various competiotns and was vice-house captian of my house.</p>],
+        direction:"left",
+        img:"./images/backpack.png"
+
+    },
+    {
+        id:4,
+        time:"2008-2016",
+        para:[<p>Always took part in various extra co-curicular activities.</p>],
+        direction:"right",
+        img:"./images/kid.png" 
+
+    },
+    {
+        id:5,
+        time:"2002",
+        para:[<p>Said "Hello World!" on 14th of January 2002.</p>],
+        direction:"left",
+        img:"./images/baby.png"
+
+    }
+];
+export default info;
